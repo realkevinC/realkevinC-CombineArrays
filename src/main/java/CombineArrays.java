43a885c1399742aa4e907f0ea72dd53b410494ce
Kevin Chen <kevinchen1688@yahoo.com>
2023-03-07 00:55:19 +0000

    done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] combine = new int [arr1.length + arr2.length];
+        int i = 0;
 
-        return null;
+        // enhance for loop
+        for (int num : arr1){
+            combine[i] = num;
+            i++;
+        }
+        for (int num : arr2){
+            combine[i] = num;
+            i++;
+        }
+        return combine;
     }
 }
